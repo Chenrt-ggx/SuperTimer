fa40386c6b447ae7ab586df32b92b3076ad31b99
Chenrt <2926777985@qq.com>
2022-07-12 23:23:23 +0800

    init

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,15 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+import 'vuetify/dist/vuetify.min.css';
+import '@mdi/font/css/materialdesignicons.min.css';
+
+Vue.use(Vuetify);
+
+export default new Vuetify({
+    breakpoint: {
+        mobileBreakpoint: 'sm'
+    },
+    icons: {
+        iconfont: 'mdiSvg'
+    }
+});
