5f48d1dd36f105988162c340bfcfb29cf5cc3a44
Chenrt <2926777985@qq.com>
2022-07-13 11:38:54 +0800

    修正 public path

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 const { defineConfig } = require('@vue/cli-service');
 
 module.exports = defineConfig({
-    publicPath: process.env.NODE_ENV === 'BuildAction' ? './' : '/',
+    publicPath: process.env.NODE_ENV === 'BuildAction' ? '/SuperTimer/' : '/',
     transpileDependencies: ['vuetify'],
     productionSourceMap: false
 });
